db1e7c8d958908498ba01d8fe08fb477c02683eb
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-01-21 23:55:03 -0400

    add button variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,34 @@
+import clsx from "clsx";
+
 interface ButtonProps {
-  children: string;
+  children: string | JSX.Element;
+  variant?: "default" | "outline";
+  square?: boolean;
+  size?: "md" | "sm";
 }
 
-export default function Button({ children }: ButtonProps) {
+export default function Button({
+  children,
+  variant,
+  square,
+  size,
+}: ButtonProps) {
   return (
-    <button className="mr-2 mb-2 rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-semibold text-white transition-colors hover:bg-blue-700 active:bg-blue-800">
+    <button
+      className={clsx(
+        "rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-semibold text-white transition-colors hover:bg-blue-700 active:bg-blue-800",
+        {
+          "border bg-transparent text-gray-500 hover:bg-gray-100  active:bg-gray-200":
+            variant === "outline",
+        },
+        {
+          "px-2.5": square === true,
+        },
+        {
+          "text-sm": size === "sm",
+        }
+      )}
+    >
       {children}
     </button>
   );
