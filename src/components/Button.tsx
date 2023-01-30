26833505fc2f01ca4511440712d11d948abaed88
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-01-30 03:03:27 -0400

    add new button type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 interface ButtonProps {
   children: string | JSX.Element;
-  variant?: "default" | "outline";
+  variant?: "default" | "outline" | "text";
   square?: boolean;
   size?: "md" | "sm";
   loading?: boolean;
@@ -29,16 +29,19 @@ export default function Button({
         "rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-semibold text-white transition-colors hover:bg-blue-700 active:bg-blue-800 disabled:bg-gray-400",
         {
           "border bg-transparent text-gray-500 hover:bg-gray-100  active:bg-gray-200":
-            variant === "outline",
+            variant === "outline" || variant === "text",
         },
         {
           "px-2.5": square === true,
         },
         {
-          "text-sm": size === "sm",
+          "px-4 py-2 text-sm": size === "sm",
         },
         {
           "border-none": noBorder === true,
+        },
+        {
+          "border-none": variant === "text",
         }
       )}
     >
