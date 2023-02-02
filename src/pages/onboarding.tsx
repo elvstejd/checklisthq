17e64789492d48381df3f54718908b08f9d315d7
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-02-01 21:37:11 -0400

    wip onboarding

---
+++ src/pages/onboarding.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Onboarding() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="mb-10 w-full max-w-2xl rounded-md p-4 py-6 md:border md:shadow-md">
+        <h1 className="mb-14 text-center text-2xl font-semibold">
+          Almost there...
+        </h1>
+      </div>
+    </div>
+  );
+}
