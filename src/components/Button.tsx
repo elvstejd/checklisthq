d46699b1cc2dcb99d84ce56ba4276c1e9f39eb85
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-02-03 01:12:06 -0400

    adjust button size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,6 @@ interface ButtonProps {
   children: string | JSX.Element;
   variant?: "default" | "outline" | "text";
   square?: boolean;
-  size?: "md" | "sm";
   loading?: boolean;
   noBorder?: boolean;
   type?: "submit" | "button" | "reset";
@@ -15,7 +14,6 @@ export default function Button({
   children,
   variant,
   square,
-  size,
   noBorder,
   onClick,
   loading,
@@ -26,16 +24,13 @@ export default function Button({
       {...{ onClick, type }}
       disabled={loading}
       className={clsx(
-        "rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-semibold text-white transition-colors hover:bg-blue-700 active:bg-blue-800 disabled:bg-gray-400",
+        "rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white transition-colors hover:bg-blue-700 active:bg-blue-800 disabled:bg-gray-400",
         {
           "border bg-transparent text-gray-800 hover:bg-gray-100  active:bg-gray-200":
             variant === "outline" || variant === "text",
         },
         {
           "px-2.5": square === true,
-        },
-        {
-          "px-4 py-2 text-sm": size === "sm",
         },
         {
           "border-none": noBorder === true,
