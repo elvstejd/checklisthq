583ba65eca6eba9d50a623ee7a3f9c66d3e3cdce
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-02-03 18:20:01 -0400

    show user's checklists on their dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 import { Eye, Pencil, Plus } from "phosphor-react";
 import Button from "../../components/Button";
 import { Shell } from "../../components/Shell";
+import { api } from "../../utils/api";
 
 export default function Dashboard() {
+  const { data: checklists } = api.checklist.getAll.useQuery();
+
   return (
     <Shell
       pageTitle="Dashboard"
@@ -12,19 +15,14 @@ export default function Dashboard() {
     >
       <h2 className="mt-8 mb-4 text-sm font-bold text-gray-400">CHECKLISTS</h2>
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <div className="flex items-center justify-between rounded-md border p-4">
-          <p className="font-medium leading-5 text-gray-600 line-clamp-3">
-            Checklist title goes here and if it is longer that
-          </p>
-          <div className="ml-4 flex gap-2 border-l pl-4">
-            <Button variant="outline" square>
-              <Pencil size={18} />
-            </Button>
-            <Button variant="outline" square>
-              <Eye size={18} />
-            </Button>
-          </div>
-        </div>
+        {checklists?.map((checklist) => (
+          <Card
+            key={checklist.id}
+            id={checklist.id}
+            title={checklist.title as string}
+          />
+        ))}
+
         <Link href="/dashboard/new">
           <button className="group flex h-full w-full border-spacing-1 items-center justify-center rounded-md border-2 border-dashed p-4 py-5 transition-colors hover:border-blue-500">
             <div className="flex items-center gap-2 font-medium text-gray-400 transition-colors group-hover:text-blue-900 ">
@@ -35,5 +33,25 @@ export default function Dashboard() {
         </Link>
       </div>
     </Shell>
+  );
+}
+
+function Card({ title, id }: { title: string; id: string }) {
+  return (
+    <div className="flex items-center justify-between rounded-md border p-4">
+      <p className="font-medium leading-5 text-gray-600 line-clamp-3">
+        {title}
+      </p>
+      <div className="ml-4 flex gap-2 border-l pl-4">
+        <Button variant="outline" square>
+          <Pencil size={18} />
+        </Button>
+        <Link href={`/${id}`}>
+          <Button variant="outline" square>
+            <Eye size={18} />
+          </Button>
+        </Link>
+      </div>
+    </div>
   );
 }
