c93d2e3c48aca16f9b401ada35224f86f5da19bf
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-02-03 16:55:53 -0400

    add settings to dashboard create, make responsive

--- src/pages/dashboard/new.tsx
+++ src/pages/dashboard/new.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ChecklistForm } from "../../components/ChecklistForm";
 import { Shell } from "../../components/Shell";
+import { useSettingsStore } from "../../stores";
+import { Toggle } from "../../components/Toggle";
 
 export default function New() {
   return (
@@ -9,7 +11,38 @@ export default function New() {
       backTo="/dashboard"
       activePath="/dashboard"
     >
-      <ChecklistForm />
+      <div className="flex flex-col md:grid md:grid-cols-12 md:gap-4">
+        <div className=" md:col-span-8 lg:col-span-9">
+          <ChecklistForm />
+        </div>
+        <div className="md:col-span-4 lg:col-span-3">
+          <Settings />
+        </div>
+      </div>
     </Shell>
+  );
+}
+
+export function Settings() {
+  const { enableMultipleSections, toggleMultipleSections } = useSettingsStore();
+
+  return (
+    <>
+      <h2 className="mt-8 mb-4 text-sm font-bold text-gray-400">SETTINGS</h2>
+      <div className="flex flex-col gap-2 rounded-md border p-4">
+        <Toggle
+          value={enableMultipleSections}
+          onChange={(value) => toggleMultipleSections(value)}
+          label="Multiple sections"
+          description="Allows you to break steps down into different sections, recommended for complex checklists."
+        />
+        <Toggle
+          value={false}
+          onChange={() => null}
+          label="Publish as temporary"
+          description="Checklist will be anonymous and deleted in 24 hours."
+        />
+      </div>
+    </>
   );
 }
