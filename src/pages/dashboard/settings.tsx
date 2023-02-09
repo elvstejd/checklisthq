87ea1e4515a55104f6ae2924ee306bcf9ba33805
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-02-09 16:15:33 -0400

    add dialog modal to settings page

--- src/pages/dashboard/settings.tsx
+++ src/pages/dashboard/settings.tsx
@@ -1,4 +1,5 @@
 import Button from "../../components/Button";
+import { DialogModal } from "../../components/DialogModal";
 import { Shell } from "../../components/Shell";
 
 export default function Settings() {
@@ -6,7 +7,13 @@ export default function Settings() {
     <Shell activePath="/dashboard/settings" pageTitle="Settings">
       <div className="mt-6">
         <p className="mb-2 text-gray-600">The only setting for now...</p>
-        <Button color="danger">Delete my account</Button>
+        <DialogModal
+          title="Are you SURE you want to delete your account?"
+          description="Not trying to gaslight you or anything, just asking if you're sure. This action is not reversible."
+          type="danger"
+          target={<Button color="danger">Delete my account</Button>}
+          onConfirm={() => console.log("confirmed. deleting...")}
+        />
       </div>
     </Shell>
   );
