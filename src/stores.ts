8e99746cf08a158a0a56bf4d8632a23838e43bda
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-02-11 18:43:30 -0400

    disable persisting state

--- src/stores.ts
+++ src/stores.ts
@@ -1,22 +1,14 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 interface SettingsStore {
   enableMultipleSections: boolean;
   toggleMultipleSections: (value: boolean) => void;
 }
 
-export const useSettingsStore = create<SettingsStore>()(
-  persist(
-    (set) => ({
-      enableMultipleSections: false,
-      toggleMultipleSections: (value) =>
-        set({
-          enableMultipleSections: value,
-        }),
+export const useSettingsStore = create<SettingsStore>()((set) => ({
+  enableMultipleSections: false,
+  toggleMultipleSections: (value) =>
+    set({
+      enableMultipleSections: value,
     }),
-    {
-      name: "settings",
-    }
-  )
-);
+}));
