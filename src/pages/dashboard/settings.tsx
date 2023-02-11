d90500ea1162679ef5d40f328ea315ced8f9ae0c
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-02-11 19:03:16 -0400

    nerf settings page

--- src/pages/dashboard/settings.tsx
+++ src/pages/dashboard/settings.tsx
@@ -6,15 +6,23 @@ export default function Settings() {
   return (
     <Shell activePath="/dashboard/settings" pageTitle="Settings">
       <div className="mt-6">
-        <p className="mb-2 text-gray-600">The only setting for now...</p>
-        <DialogModal
-          title="Are you SURE you want to delete your account?"
-          description="Not trying to gaslight you or anything, just asking if you're sure. This action is not reversible."
-          type="danger"
-          target={<Button color="danger">Delete my account</Button>}
-          onConfirm={() => console.log("confirmed. deleting...")}
-          confirmLabel="Yes, delete forever."
-        />
+        <div className="text-gray-400">
+          <p className="mb-2 ">Coming soon:</p>
+          <ul className="list-disc">
+            <li>Set my profile as public</li>
+            <li>Delete my account</li>
+          </ul>
+        </div>
+        <div className="hidden">
+          <DialogModal
+            title="Are you sure you want to delete your account?"
+            description="This action is not reversible. All your checklist will be gone as well. Unless they were created as an"
+            type="danger"
+            target={<Button color="danger">Delete my account</Button>}
+            onConfirm={() => console.log("confirmed. deleting...")}
+            confirmLabel="Yes, delete forever."
+          />
+        </div>
       </div>
     </Shell>
   );
