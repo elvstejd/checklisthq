725d9ff7dd81dc6f85cda494769847d9f953ce72
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-02-18 16:04:07 -0400

    add correct loading skeleton for profile page

--- src/pages/u/[username].tsx
+++ src/pages/u/[username].tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import { TRPCClientError } from "@trpc/client";
-import clsx from "clsx";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
@@ -53,34 +52,38 @@ const UserProfile: NextPage = () => {
           <div className="mx-auto max-w-2xl px-4">
             {error && <ErrorFrame message={error} />}
             {!profile && !error && <LoadingSkeleton />}
-            <div className="h-32 rounded-lg bg-gray-200"></div>
-            <div className="-mt-7 flex flex-col items-center justify-center">
-              <img
-                src={profile?.image ?? "hello"}
-                className="aspect-square h-16 rounded-full border-4 border-white object-cover"
-                alt="profile picture"
-              />
-              <p className="text-xl font-bold md:text-2xl">
-                {profile?.username}
-              </p>
-              <p className="mt-1 rounded-md bg-slate-100 p-1 px-2 text-sm text-slate-800">
-                {profile?.Checklists.length} checklists
-              </p>
-            </div>
-            <div className="mt-10 flex flex-col gap-4">
-              {profile?.Checklists.map((checklist) => (
-                <Link
-                  href={`/${checklist.id}`}
-                  target="_blank"
-                  key={checklist.id}
-                >
-                  <div className="group flex items-center rounded-md border px-4 py-2">
-                    <span className="flex-grow"> {checklist.title}</span>
-                    <ArrowRight className="transition-colors group-hover:text-blue-600" />
-                  </div>
-                </Link>
-              ))}
-            </div>
+            {profile && (
+              <>
+                <div className="h-32 rounded-lg bg-gray-100"></div>
+                <div className="-mt-7 flex flex-col items-center justify-center">
+                  <img
+                    src={profile?.image ?? "hello"}
+                    className="aspect-square h-16 rounded-full border-4 border-white object-cover"
+                    alt="profile picture"
+                  />
+                  <p className="text-xl font-bold md:text-2xl">
+                    {profile?.username}
+                  </p>
+                  <p className="mt-1 rounded-md bg-slate-100 p-1 px-2 text-sm text-slate-800">
+                    {profile?.Checklists.length} checklists
+                  </p>
+                </div>
+                <div className="mt-10 flex flex-col gap-3">
+                  {profile?.Checklists.map((checklist) => (
+                    <Link
+                      href={`/${checklist.id}`}
+                      target="_blank"
+                      key={checklist.id}
+                    >
+                      <div className="group flex items-center rounded-md border px-4 py-2">
+                        <span className="flex-grow"> {checklist.title}</span>
+                        <ArrowRight className="transition-colors group-hover:text-blue-600" />
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+              </>
+            )}
           </div>
         </main>
         <div>
@@ -95,31 +98,22 @@ export default UserProfile;
 
 function LoadingSkeleton() {
   return (
-    <div role="status" className="animate-pulse">
-      <div className="flex justify-center">
-        <div className="mx-24 mb-4 h-8 w-full rounded-md bg-gray-200"></div>
+    <>
+      <div className="animate-pulse h-32 rounded-lg bg-gray-100"></div>
+      <div className="animate-pulse -mt-7 flex flex-col items-center justify-center">
+        <div className="animate-pulse aspect-square h-16 rounded-full border-4 border-white bg-gray-100" />
+        <p className="animate-pulse h-7 w-40 rounded-md bg-gray-100 text-xl font-bold md:text-2xl"></p>
+        <p className="animate-pulse mt-1 h-6 w-28 rounded-md bg-slate-100 p-1 px-2 text-sm text-slate-800"></p>
       </div>
-
-      <div className="mb-6 rounded-md border border-solid border-gray-200 px-4">
-        {["", "", "", "", "", ""].map((_, k) => (
+      <div className="mt-10 flex flex-col gap-2">
+        {["", "", ""].map((_, idx) => (
           <div
-            key={k}
-            className={clsx(
-              "group flex cursor-pointer items-center gap-4 border-t-gray-200 py-3",
-              k !== 0 ? "border-t" : "border-t-0"
-            )}
-          >
-            <div className="flex aspect-square h-10 items-center justify-center rounded-full bg-gray-200"></div>
-            <div className="w-full">
-              <div className="h-4 w-24 rounded-sm bg-gray-200"></div>
-              <div className="mt-1 h-8 w-48 rounded-sm bg-gray-200"></div>
-            </div>
-          </div>
+            key={idx}
+            className="animate-pulse group flex h-10 items-center rounded-md bg-gray-100"
+          ></div>
         ))}
       </div>
-
-      <span className="sr-only">Loading...</span>
-    </div>
+    </>
   );
 }
 
